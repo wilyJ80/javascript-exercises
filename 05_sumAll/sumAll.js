be6634a19b3b07ea423136529742e3713f4ecae8
wilyJ80 <abjurandam@gmail.com>
2024-01-04 16:11:29 -0300

    Pass half tests of 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(bound1, bound2) {
+
+  let first = 0; last = 0;
+  if (bound1 > bound2) { last = bound1; first = bound2;}
+  else if (bound2 > bound1) { first = bound1; last = bound2;}
+  else { return bound1; }
+
+  let sum = 0;
+  for (let i = first; i <= last; i++) {
+    sum += i;
+  }
+
+  return sum;
 
 };
 

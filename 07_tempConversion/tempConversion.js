b573b4168f21a8c12b25cd4addd3d7baaa7995a7
wilyJ80 <abjurandam@gmail.com>
2024-01-04 19:06:28 -0300

    Finish 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+
+  const converted = ((fTemp-32)*(5/9));
+  return (Math.round(converted * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+
+  const converted = ((cTemp*9/5)+32);
+  return (Math.round(converted * 10) / 10);
 };
 
 // Do not edit below this line

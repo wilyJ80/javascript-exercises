3827a720d34266b614b389745cec7720cb671058
wilyJ80 <abjurandam@gmail.com>
2024-01-14 16:07:32 -0300

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	const pStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+	return pStr.split("").reverse().join("") === pStr;
 };
 
 // Do not edit below this line
